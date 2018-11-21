019fab569112b6a161d71738aebc13a01e7257b1
Wook Song <wook16.song@samsung.com>
2018-11-21 17:25:19 +0900

    [Common/RosBridge] Add a bridge for ROS support in NNStreamer
    This patch introduces nns_ros_bridge, a bridge for ROS support, to the
    NNStreamer framework. In order to publish and subscribe 'tensors'
    between the nns_ros_bridge-based nodes, the custom message named
    'tensors' is newly defined using UInt8MultiArray of std_msgs.
    
    Signed-off-by: Wook Song <wook16.song@samsung.com>

---
+++ common/include/nns_ros_bridge.h
@@ -0,0 +1,71 @@
+/**
+ * Copyright (C) 2018 Samsung Electronics Co., Ltd. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ */
+/**
+ * @file   nns_ros_bridge.h
+ * @author Wook Song <wook16.song@samsung.com>
+ * @date   11/19/2018
+ * @brief  A bridge for ROS support within NNStreamer
+ *
+ * This class bridges between the NNStreamer (C) and ROS frameworks (ROSCPP/C++).
+ *
+ * @bug     No known bugs.
+ */
+
+#ifndef _NNS_ROS_BRIDGE_H_
+#define _NNS_ROS_BRIDGE_H_
+#include <glib-2.0/glib.h>
+#include <nnstreamer/tensor_common.h>
+#ifdef __cplusplus
+#include <ros/ros.h>
+#include <std_msgs/MultiArrayLayout.h>
+#include <std_msgs/MultiArrayDimension.h>
+
+class NnsRosBridge
+{
+public:
+  NnsRosBridge (const char *node_name, const char *topic_name);
+  gboolean publish (const guint num_tensors,
+      const GstTensorMemory *tensors_mem);
+  gboolean setPubTopicInfo (const GstTensorsConfig *conf);
+private:
+  NnsRosBridge ();
+  ~NnsRosBridge ();
+
+  // Variables for ROS configuration
+  ros::NodeHandle *nh_parent;
+  ros::NodeHandle *nh_child;
+  ros::Publisher  ros_sink_pub;
+  std::string str_node_name;
+  std::string str_pub_topic_name;
+  // Variables for publish ROS topic
+  gboolean ready_to_pub;
+  guint num_of_tensors_pub;
+  std_msgs::MultiArrayLayout topic_layouts_pub[NNS_TENSOR_SIZE_LIMIT];
+};
+
+extern "C"
+{
+#endif /* __cplusplus */
+
+void *nns_ros_bridge_init (const char *node_name, const char *topic_name);
+gboolean nns_ros_bridge_publish (void *instance, const guint num_tensors,
+    const GstTensorMemory *tensors_mem);
+gboolean nns_ros_bridge_set_pub_topic (void *instance,
+    const GstTensorsConfig *conf);
+
+#ifdef __cplusplus
+};
+#endif /* __cplusplus */
+#endif /* _NNS_ROS_BRIDGE_H_ */
---
+++ common/node/nns_ros_bridge.cc
@@ -0,0 +1,170 @@
+/**
+ * Copyright (C) 2018 Samsung Electronics Co., Ltd. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ */
+/**
+ * @file   nns_ros_bridge.cc
+ * @author Wook Song <wook16.song@samsung.com>
+ * @date   11/19/2018
+ * @brief  A bridge for ROS support within NNStreamer
+ *
+ * This class bridges between the NNStreamer (C) and ROS frameworks (ROSCPP/C++).
+ *
+ * @bug     No known bugs.
+ */
+#include <ros/ros.h>
+#include <std_msgs/MultiArrayLayout.h>
+#include <std_msgs/MultiArrayDimension.h>
+
+#include "nns_ros_bridge.h"
+#include "nns_ros_bridge/tensors.h"
+
+const char BASE_NODE_NAME[] = "tensor_ros_sink";
+const guint DEFAULT_Q_SIZE = 15;
+
+/**
+ * @brief	The only public constructor of this class
+ * @param[in] node_name : The name of the ROS node under /tensor_ros_sink
+ * @param[in]  topic_name : The name of topic what this class concerns
+ * @return None
+ */
+NnsRosBridge::NnsRosBridge (const char *node_name, const char *topic_name)
+{
+  /* ROS initialization requires command-line arguments */
+  int dummy_argc = 0;
+  char **dummy_argv = NULL;
+
+  this->str_node_name = std::string (node_name);
+  this->str_pub_topic_name = std::string (topic_name);
+
+  ros::init (dummy_argc, dummy_argv, this->str_node_name);
+  this->nh_parent = new ros::NodeHandle (BASE_NODE_NAME);
+  this->nh_child = new ros::NodeHandle (*(this->nh_parent),
+      this->str_node_name);
+
+  this->ready_to_pub = FALSE;
+
+  /**
+   * The name of published topic would be
+   * /tensor_ros_sink/PID_${PID}/${ElementNameOfTensorRosSink}
+   */
+  this->ros_sink_pub = this->nh_child->advertise<nns_ros_bridge::tensors>(
+      this->str_pub_topic_name, DEFAULT_Q_SIZE);
+}
+
+/**
+ * @brief	A method for configuraing topic information to publish
+ * @param[in] conf : The configuration information at the NNStreamer side
+ * @return TRUE if the configuration is successfully done
+ */
+gboolean NnsRosBridge::setPubTopicInfo (const GstTensorsConfig *conf)
+{
+  const GstTensorsInfo *tensors_info = &conf->info;
+
+  g_return_val_if_fail (tensors_info->num_tensors <= NNS_TENSOR_SIZE_LIMIT,
+    FALSE);
+
+  this->num_of_tensors_pub = tensors_info->num_tensors;
+
+  for (guint i = 0; i < this->num_of_tensors_pub; ++i) {
+    gsize tensor_size = gst_tensor_info_get_size (&tensors_info->info[i]);
+
+    for (guint j = 0; j < NNS_TENSOR_RANK_LIMIT; ++j) {
+      std_msgs::MultiArrayDimension each_dim;
+      /**
+       * FIXME: Since the type of 'stride' of std_msgs::MultiArrayDimension is
+       * unsigned int,conversion from size_t to unsigned int is requried.
+       */
+      guint each_dim_stride = 1;
+
+      for (guint k = 0; k <= NNS_TENSOR_RANK_LIMIT - (j + 1); ++k) {
+        each_dim_stride *= tensors_info->info[i].dimension[k];
+      }
+      each_dim.stride = each_dim_stride;
+      each_dim.label = tensor_element_typename[tensors_info->info[i].type];
+      each_dim.size =
+          tensors_info->info[i].dimension[NNS_TENSOR_RANK_LIMIT - (j + 1)];
+
+      this->topic_layouts_pub[i].dim.push_back (each_dim);
+    }
+
+    g_return_val_if_fail (
+        this->topic_layouts_pub[i].dim.at(0).stride == tensor_size, FALSE
+        );
+  }
+
+  this->ready_to_pub = TRUE;
+
+  return ready_to_pub;
+}
+
+/**
+ * @brief	A method for publishing a ROS topic that contains the tensors passed by the NNStreamer framework
+ * @param[in] num_tensors : The number of tensors included in tensors_mem (for the verification purpose)
+ * @param[in] tensors_mem : The pointer of containers consists of information and raw data of tensors
+ * @return TRUE if the configuration is successfully done
+ */
+gboolean NnsRosBridge::publish (const guint num_tensors,
+    const GstTensorMemory *tensors_mem)
+{
+  nns_ros_bridge::tensors tensors_msg;
+
+  g_return_val_if_fail (ros::ok(), FALSE);
+  g_return_val_if_fail (this->ready_to_pub, FALSE);
+  g_return_val_if_fail (num_tensors == this->num_of_tensors_pub, FALSE);
+
+  for (guint i = 0; i < this->num_of_tensors_pub; ++i) {
+    std_msgs::UInt8MultiArray each_tensor;
+    uint8_t *src_data = (uint8_t *) tensors_mem[i].data;
+
+    each_tensor.layout = this->topic_layouts_pub[i];
+    each_tensor.data.assign (src_data, src_data + tensors_mem[i].size);
+
+    tensors_msg.tensors.push_back (each_tensor);
+  }
+
+  this->ros_sink_pub.publish (tensors_msg);
+  ros::spinOnce();
+
+  return TRUE;
+}
+
+/**
+ * C functions for NNStreamer plugins that want to publish or subscribe
+ * ROS topics. nns_ros_bridge_init() should be invoked before configuring the
+ * tensors information to publish or subscribe. Each NNStreamer plugin which
+ * uses this class should be holding the instance of NnsRosBridge (i.e., the
+ * void type pointer what the nns_ros_bridge_init() function returns).
+ */
+void *
+nns_ros_bridge_init (const char *node_name, const char *topic_name)
+{
+  return new NnsRosBridge (node_name, topic_name);
+}
+
+gboolean
+nns_ros_bridge_publish (void *instance, const guint num_tensors,
+    const GstTensorMemory *tensors_mem)
+{
+  NnsRosBridge *nrb_instance = (NnsRosBridge *) instance;
+
+  return nrb_instance->publish (num_tensors, tensors_mem);
+}
+
+gboolean
+nns_ros_bridge_set_pub_topic (void *instance, const GstTensorsConfig *conf)
+{
+  NnsRosBridge *nrb_instance = (NnsRosBridge *) instance;
+
+  return nrb_instance->setPubTopicInfo (conf);
+}
