0b290fc0783243088dd2972377f452f92147faa5
Sangjung Woo <sangjung.woo@samsung.com>
2019-03-06 17:55:10 +0900

    [Src] Add RosSrc element for subscribing Ros topic
    This patch newly adds the RosSrc element, which fetches the Ros message
    from the designated topic and provides tensor stream with gstreamer
    pipeline.
    
    Related parameter is as below.
    * topic: ROS Topic Name for subscription
    * freqrate: Frequency rate for checking Ros Topic (Hz, Default: 1Hz)
    * datatype: Primitive datatype of target ROS topic
    * input: Input dimension of target ROS topic (e.g. 10:1:1:1)
    
    Signed-off-by: Sangjung Woo <sangjung.woo@samsung.com>

---
+++ gst/tensor_ros_src/tensor_ros_src.h
@@ -0,0 +1,79 @@
+/**
+ * GStreamer
+ * Copyright (C) 2005 Thomas Vander Stichele <thomas@apestaart.org>
+ * Copyright (C) 2005 Ronald S. Bultje <rbultje@ronald.bitfreak.net>
+ * Copyright (C) 2019 Samsung Electronics Co., Ltd.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ */
+
+/**
+ * @file	tensor_ros_src.h
+ * @date	03/06/2019
+ * @brief	GStreamer plugin to handle tensor stream from Ros topic
+ * @see		https://github.com/nnsuite/nnstreamer
+ * @see		https://github.com/nnsuite/nnstreamer-ros
+ * @author	Sangjung Woo <sangjung.woo@samsung.com>
+ * @bug		No known bugs except for NYI items
+ */
+
+#ifndef __GST_TENSOR_ROS_SRC_H__
+#define __GST_TENSOR_ROS_SRC_H__
+
+#include <gst/gst.h>
+#include <gst/base/gstpushsrc.h>
+
+#include "nns_ros_subscriber.h"
+
+G_BEGIN_DECLS
+
+#define GST_TYPE_TENSOR_ROS_SRC \
+  (gst_tensor_ros_src_get_type())
+#define GST_TENSOR_ROS_SRC(obj) \
+  (G_TYPE_CHECK_INSTANCE_CAST((obj),GST_TYPE_TENSOR_ROS_SRC,GstTensorRosSrc))
+#define GST_TENSOR_ROS_SRC_CLASS(klass) \
+  (G_TYPE_CHECK_CLASS_CAST((klass),GST_TYPE_TENSOR_ROS_SRC,GstTensorRosSrcClass))
+#define GST_IS_TENSOR_ROS_SRC(obj) \
+  (G_TYPE_CHECK_INSTANCE_TYPE((obj),GST_TYPE_TENSOR_ROS_SRC))
+#define GST_IS_TENSOR_ROS_SRC_CLASS(klass) \
+  (G_TYPE_CHECK_CLASS_TYPE((klass),GST_TYPE_TENSOR_ROS_SRC))
+
+typedef struct _GstTensorRosSrc GstTensorRosSrc;
+typedef struct _GstTensorRosSrcClass GstTensorRosSrcClass;
+
+struct _GstTensorRosSrc
+{
+  GstPushSrc parent;
+
+  GstCaps *caps;
+  gboolean silent;
+  GThread *thread;        /** ros subscribe thread */
+
+  gchar *topic_name;      /** ROS topic name to subscribe */
+  gchar *input_dims;      /** input dimemsion of ROS message */
+  guint count;            /** total item count of ROS message */
+  tensor_type datatype;   /** Primitive datatype of ROS topic */
+  gulong freq_rate;       /** frequency rate to check */
+
+  GAsyncQueue *queue;     /** data queue */
+  class NnsRosSubscriber *ros_sub;    /** NnsRosSubscriber instance for subscribing Ros topic in other thread */
+};
+
+struct _GstTensorRosSrcClass 
+{
+  GstPushSrcClass parent_class;
+};
+
+GType gst_ros_src_get_type (void);
+
+G_END_DECLS
+
+#endif /* __GST_TENSOR_ROS_SRC_H__ */
