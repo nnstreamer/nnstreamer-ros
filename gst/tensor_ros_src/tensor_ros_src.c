d84144b8ce7beb9f807b72ad3521d95211615735
Wook Song <wook16.song@samsung.com>
2020-11-02 19:14:06 +0900

    [Ros/Src] Refactor tensor_ros_src for ROS1/RosCpp
    This patch revises the implementation of tensor_ros_src for ROS1
    (ROSCPP) as follows:
    - Moved the common code that can be used for both ROS1 and ROS2 to a
    virtual class, NnsRosSubscriber
    - Instead of tensor_ros_listener.hpp, the header which is generated from
    the ROS message definition is used.
    - Removed duplicate code in the CMake script.
    
    Signed-off-by: Wook Song <wook16.song@samsung.com>

---
+++ gst/tensor_ros_src/tensor_ros_src.c
@@ -0,0 +1,512 @@
+/* SPDX-License-Identifier: LGPL-2.1-only */
+/**
+ * NNStreamer-ROS: NNStreamer extension packages for ROS/ROS2 support
+ * Copyright (C) 2019 Sangjung Woo <sangjung.woo@samsung.com>
+ * Copyright (C) 2020 Wook Song <wook16.song@samsung.com>
+ * Copyright (C) 2020 Samsung Electronics Co., Ltd.
+ *
+ * @file        tensor_ros_src.c
+ * @date        10/20/2020
+ * @brief       GStreamer plugin to subscribe a Ros topic and convert them into
+ *              tensor stream
+ * @see         https://github.com/nnstreamer/nnstreamer-ros
+ * @author      Sangjung Woo <sangjung.woo@samsung.com>
+ *              Wook Song <wook16.song@samsung.com>
+ * @bug         No known bugs except for NYI items
+ *
+ * This class bridges between the NNStreamer (C) and ROS2 frameworks (RCLCPP/C++)
+ * by implementing the NnsRosPublisher class.
+ *
+ */
+/**
+ * SECTION:element-tensor_ros_src
+ *
+ * NNStreamer-ROS: NNStreamer extension packages for ROS/ROS2 support
+ * tensor_ros_src and tensor_ros2_src:
+ *    Source element to publish tensor stream from deginated Ros topic
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#ifdef G_OS_WIN32
+#include <process.h>
+#else
+#include <sys/types.h>
+#include <unistd.h>
+#endif
+
+#include <nnstreamer/tensor_typedef.h>
+#include <nnstreamer/nnstreamer_plugin_api.h>
+#include <string.h>
+
+#include "nns_ros_subscriber.h"
+#include "tensor_ros_src.h"
+//#include "tensor_ros_listener.hpp"
+
+#ifdef WITH_ROS1
+#define NNS_NAME_TENSOR_ROS_SRC  tensor_ros_src
+#else   /** WITH_ROS2 */
+#define NNS_NAME_TENSOR_ROS_SRC  tensor_ros2_src
+#endif  /** WITH_ROS1 */
+
+/**
+ * @brief Macro for debug mode.
+ */
+#ifndef DBG
+#define DBG (!rossrc->silent)
+#endif
+
+/**
+ * @brief Macro for debug message.
+ */
+#define silent_debug(SELF, ...) do { \
+    if (DBG) { \
+      GST_DEBUG_OBJECT (SELF, __VA_ARGS__); \
+    } \
+  } while (0)
+
+GST_DEBUG_CATEGORY_STATIC (gst_tensor_ros_src_debug);
+#define GST_CAT_DEFAULT gst_tensor_ros_src_debug
+
+#define DEFAULT_LIVE_MODE       TRUE
+
+/**
+ * @brief Flag to print minimized log.
+ */
+#define DEFAULT_SILENT TRUE
+
+/**
+ * @brief @todo
+ */
+#define DEFAULT_TIMEOUT 5.0
+
+/**
+ * @brief tensor_ros_src properties
+ */
+enum
+{
+  PROP_0,
+  PROP_SILENT,      /*<< Slient mode for debug */
+  PROP_TOPIC,       /*<< ROS topic name to subscribe */
+  PROP_RATE,        /*<< Frequency rate to check the published topic */
+  PROP_TIMEOUT,     /*<< Timeout in seconds waiting for the next message to receive */
+};
+
+/**
+ * @brief Template for ros source pad
+ */
+static GstStaticPadTemplate src_pad_template =
+GST_STATIC_PAD_TEMPLATE ("src",
+    GST_PAD_SRC,
+    GST_PAD_ALWAYS,
+    GST_STATIC_CAPS (GST_TENSOR_CAP_DEFAULT "; " GST_TENSORS_CAP_DEFAULT));
+
+#define gst_tensor_ros_src_parent_class parent_class
+G_DEFINE_TYPE (GstTensorRosSrc, gst_tensor_ros_src, GST_TYPE_PUSH_SRC);
+
+/** GObject method implementation */
+static void gst_tensor_ros_src_set_property (GObject * object, guint prop_id,
+    const GValue * value, GParamSpec * pspec);
+static void gst_tensor_ros_src_get_property (GObject * object, guint prop_id,
+    GValue * value, GParamSpec * pspec);
+static void gst_tensor_ros_src_dispose (GObject * object);
+
+/** GstBaseSrc method implementation */
+static gboolean
+gst_tensor_ros_src_start (GstBaseSrc * src);
+static gboolean
+gst_tensor_ros_src_stop (GstBaseSrc * src);
+static GstCaps *
+gst_tensor_ros_src_get_caps (GstBaseSrc * src, GstCaps * filter);
+static gboolean
+gst_tensor_ros_src_set_caps (GstBaseSrc * src, GstCaps * caps);
+static GstCaps *
+gst_tensor_ros_src_fixate (GstBaseSrc * src, GstCaps * caps);
+
+/** GstPushSrc method implementation */
+static GstFlowReturn
+gst_tensor_ros_src_create (GstPushSrc * src, GstBuffer ** buffer);
+
+/**
+ *  @brief Set the live opreation mode for live source
+ */
+static void
+set_live_mode (GstBaseSrc * src, gboolean mode)
+{
+  if (gst_base_src_is_live (src) == mode)
+    return;
+
+  gst_base_src_set_live (src, mode);
+  gst_base_src_set_do_timestamp (src, mode);
+}
+
+/**
+ * @brief initialize the rossrc's class
+ */
+static void
+gst_tensor_ros_src_class_init (GstTensorRosSrcClass * klass)
+{
+  GObjectClass *gobject_class = G_OBJECT_CLASS (klass);
+  GstElementClass *gstelement_class = GST_ELEMENT_CLASS (klass);
+  GstBaseSrcClass *gstbasesrc_class = GST_BASE_SRC_CLASS (klass);
+  GstPushSrcClass *gstpushsrc_class = GST_PUSH_SRC_CLASS (klass);
+
+  /** GObject methods */
+  gobject_class->set_property = gst_tensor_ros_src_set_property;
+  gobject_class->get_property = gst_tensor_ros_src_get_property;
+  gobject_class->dispose = gst_tensor_ros_src_dispose;
+
+  /** GstBaseSrc method */
+  gstbasesrc_class->start = GST_DEBUG_FUNCPTR (gst_tensor_ros_src_start);
+  gstbasesrc_class->stop = GST_DEBUG_FUNCPTR (gst_tensor_ros_src_stop);
+  gstbasesrc_class->set_caps = GST_DEBUG_FUNCPTR (gst_tensor_ros_src_set_caps);
+  gstbasesrc_class->get_caps = GST_DEBUG_FUNCPTR (gst_tensor_ros_src_get_caps);
+  gstbasesrc_class->fixate = GST_DEBUG_FUNCPTR (gst_tensor_ros_src_fixate);
+
+  /** GstPushSrc method */
+  gstpushsrc_class->create = gst_tensor_ros_src_create;
+
+  /** Install properties */
+  /**
+   * GstTensorRosSrc::silent:
+   *
+   * The flag to enable/disable debugging messages.
+   */
+  g_object_class_install_property (gobject_class, PROP_SILENT,
+      g_param_spec_boolean ("silent", "Silent", "Produce verbose output",
+          FALSE, G_PARAM_READWRITE));
+
+  /**
+   * GstTensorRosSrc::topic:
+   *
+   * The topic name that this src element subscribes.
+   */
+  g_object_class_install_property (gobject_class, PROP_TOPIC,
+      g_param_spec_string ("topic", "Topic",
+          "ROS Topic Name for subscription", "",
+          (GParamFlags)(G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS)));
+
+  g_object_class_install_property (gobject_class, PROP_RATE,
+      g_param_spec_double ("rate", "Rate",
+          "The desired rate to run at in Hz (Default: 1).",
+          (gdouble) 1, (gdouble) G_USEC_PER_SEC, (gdouble) 1,
+          G_PARAM_READWRITE));
+
+  g_object_class_install_property (gobject_class, PROP_TIMEOUT,
+      g_param_spec_double ("timeout", "Timeout",
+          "Timeout in seconds waiting for the next message to receive (Default: 5).",
+          (gdouble) 1, (gdouble) 600, (gdouble) DEFAULT_TIMEOUT,
+          G_PARAM_READWRITE));
+
+  gst_element_class_add_static_pad_template (gstelement_class,
+      &src_pad_template);
+
+  gst_element_class_set_static_metadata (gstelement_class,
+      "TensorRosSrc",
+      "Source/Tensor",
+      "Source element to get the topic data from ROS node",
+      "Samsung Electronics Co., Ltd.");
+}
+
+/**
+ * @brief Initialize ensor_ros_src element
+ */
+static void
+gst_tensor_ros_src_init (GstTensorRosSrc * rossrc)
+{
+  /* set the default properties */
+  rossrc->silent = DEFAULT_SILENT;
+  rossrc->topic_name = NULL;
+  rossrc->rate = (gdouble)  1;
+  rossrc->timeout = (gdouble) DEFAULT_TIMEOUT;
+  rossrc->queue = NULL;
+  rossrc->configured = FALSE;
+
+  /* set live mode as default */
+  set_live_mode (GST_BASE_SRC (rossrc), DEFAULT_LIVE_MODE);
+  gst_base_src_set_format (GST_BASE_SRC (rossrc), GST_FORMAT_TIME);
+}
+
+/**
+ * @brief Dispose allocated resources
+ */
+static void
+gst_tensor_ros_src_dispose (GObject * object)
+{
+  GstTensorRosSrc *rossrc = GST_TENSOR_ROS_SRC (object);
+
+  if (rossrc->queue)
+    nns_ros_subscriber_put_queue (rossrc->nns_ros_bind_instance);
+
+  if (rossrc->topic_name)
+    g_free (rossrc->topic_name);
+  rossrc->configured = FALSE;
+
+  nns_ros_subscriber_fianlize (rossrc->nns_ros_bind_instance);
+
+  G_OBJECT_CLASS (parent_class)->dispose (object);
+}
+
+/**
+ * @brief Start handler
+ */
+static gboolean
+gst_tensor_ros_src_start (GstBaseSrc * src)
+{
+  static const gchar format_node_name_pid[] = "PID_%d_%s";
+  GstTensorRosSrc *rossrc;
+  gchar *name_node;
+  gint32 pid;
+
+  rossrc = GST_TENSOR_ROS_SRC (src);
+  g_return_val_if_fail (GST_IS_TENSOR_ROS_SRC (rossrc), FALSE);
+  if ((rossrc->topic_name == NULL) || (strlen (rossrc->topic_name) == 0)) {
+    g_critical (
+        "ERROR: Failed to start %s: topic to subscribe should be given.",
+        GST_ELEMENT_NAME (GST_ELEMENT (src)));
+    return FALSE;
+  }
+
+  pid = getpid ();
+  name_node = g_strdup_printf (format_node_name_pid, pid,
+      GST_ELEMENT_NAME (GST_ELEMENT (src)));
+
+  rossrc->nns_ros_bind_instance = nns_ros_subscriber_init (name_node,
+      rossrc->topic_name, rossrc->rate, rossrc->timeout, &rossrc->configs,
+      FALSE);
+  g_free (name_node);
+
+  g_return_val_if_fail (rossrc->nns_ros_bind_instance != NULL, FALSE);
+
+  rossrc->queue = nns_ros_subscriber_get_queue (rossrc->nns_ros_bind_instance);
+
+  rossrc->configured = TRUE;
+  gst_base_src_start_complete (src, GST_FLOW_OK);
+
+  return TRUE;
+}
+
+/**
+ * @brief Stop handler
+ */
+static gboolean
+gst_tensor_ros_src_stop (GstBaseSrc * src)
+{
+  GstTensorRosSrc *rossrc = GST_TENSOR_ROS_SRC (src);
+
+  silent_debug (rossrc, "stop RosSubscriber");
+
+  return TRUE;
+}
+
+/**
+ * @brief Get caps handler
+ */
+static GstCaps *
+gst_tensor_ros_src_get_caps (GstBaseSrc * src, GstCaps * filter)
+{
+  GstCaps *caps;
+
+  caps = gst_pad_get_current_caps (src->srcpad);
+  if (caps == NULL)
+    caps = gst_pad_get_pad_template_caps (src->srcpad);
+
+  if (filter) {
+    GstCaps *intersection;
+    intersection = gst_caps_intersect_full (filter, caps, GST_CAPS_INTERSECT_FIRST);
+    gst_caps_unref (caps);
+    caps = intersection;
+  }
+
+  return caps;
+}
+
+/**
+ * @brief Set caps handler
+ */
+static gboolean
+gst_tensor_ros_src_set_caps (GstBaseSrc * src, GstCaps * caps)
+{
+  return gst_pad_set_caps (src->srcpad, caps);
+}
+
+static GstCaps *
+gst_tensor_ros_src_fixate (GstBaseSrc * src, GstCaps * caps)
+{
+  GstTensorRosSrc *rossrc = GST_TENSOR_ROS_SRC (src);
+  GstCaps *fixated_caps;
+  GstCaps *update_caps;
+  GstCaps *ret_caps;
+
+  if (rossrc->configs.info.num_tensors == 1) {
+    GstTensorConfig tensor_config;
+
+    gst_tensor_info_copy (&tensor_config.info,
+        &(rossrc->configs.info.info[0]));
+    tensor_config.rate_n = rossrc->configs.rate_n;
+    tensor_config.rate_d = rossrc->configs.rate_d;
+    fixated_caps = gst_tensor_caps_from_config (&tensor_config);
+    gst_tensor_info_free (&tensor_config.info);
+  } else {
+    fixated_caps = gst_tensors_caps_from_config (&rossrc->configs);
+  }
+
+  if (fixated_caps == NULL) {
+    GST_ERROR_OBJECT (rossrc, "Error creating fixated caps from config.");
+    return NULL;
+  }
+
+  update_caps = gst_caps_intersect (caps, fixated_caps);
+  if (!update_caps) {
+    GST_ERROR_OBJECT (rossrc,
+        "No intersection while fixating caps of the element.");
+    ret_caps = NULL;
+  } else {
+    ret_caps =gst_caps_fixate (update_caps);
+  }
+
+  gst_caps_unref (caps);
+  gst_caps_unref (fixated_caps);
+
+  return ret_caps;
+}
+
+/**
+ * @brief Setter for tensor_ros_src properties
+ */
+static void
+gst_tensor_ros_src_set_property (GObject * object, guint prop_id,
+    const GValue * value, GParamSpec * pspec)
+{
+  GstTensorRosSrc *rossrc = GST_TENSOR_ROS_SRC (object);
+
+  switch (prop_id) {
+    case PROP_SILENT:
+      rossrc->silent = g_value_get_boolean (value);
+      silent_debug (rossrc, "Silent Mode: %s\n",
+          rossrc->silent ? "true" : "false");
+      break;
+
+    case PROP_TOPIC:
+      rossrc->topic_name = g_value_dup_string (value);
+      silent_debug (rossrc, "topic name: %s\n", rossrc->topic_name);
+      break;
+
+    case PROP_RATE:
+      rossrc->rate = g_value_get_double (value);
+      silent_debug (rossrc, "Rate in Hz: %lf\n", rossrc->rate);
+      break;
+
+    case PROP_TIMEOUT:
+      rossrc->timeout = g_value_get_double (value);
+      silent_debug (rossrc, "Timeout in seconds: %lf\n", rossrc->timeout);
+      break;
+
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+      break;
+  }
+}
+
+/**
+ * @brief Getter for tensor_ros_src properties
+ */
+static void
+gst_tensor_ros_src_get_property (GObject * object, guint prop_id,
+    GValue * value, GParamSpec * pspec)
+{
+  GstTensorRosSrc *rossrc = GST_TENSOR_ROS_SRC (object);
+
+  switch (prop_id) {
+    case PROP_SILENT:
+      g_value_set_boolean (value, rossrc->silent);
+      break;
+
+    case PROP_TOPIC:
+      g_value_set_string (value, rossrc->topic_name);
+      break;
+
+    case PROP_RATE:
+      g_value_set_double (value, rossrc->rate);
+      break;
+
+    case PROP_TIMEOUT:
+      g_value_set_double (value, rossrc->timeout);
+      break;
+
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+      break;
+  }
+}
+
+/**
+ * @brief Push GstBuffer which contains the subscribed ROS data
+ */
+static GstFlowReturn
+gst_tensor_ros_src_create (GstPushSrc * src, GstBuffer ** buffer)
+{
+  GstTensorRosSrc *rossrc = GST_TENSOR_ROS_SRC (src);
+  const guint64 timeout = rossrc->timeout * G_USEC_PER_SEC;
+  const guint num_tensors = rossrc->configs.info.num_tensors;
+  GstBuffer *buf = NULL;
+  GstMemory *mem = NULL;
+  gpointer data_tensors = NULL;
+  gsize size_tensors = 0;
+  guint i;
+
+  for (i = 0; i < num_tensors; ++i) {
+    gsize size = gst_tensor_info_get_size (&(rossrc->configs.info.info[i]));
+
+    size_tensors += size;
+  }
+
+  data_tensors = g_async_queue_timeout_pop (rossrc->queue, timeout);
+  if (!data_tensors)
+    return GST_FLOW_EOS;
+
+  buf = gst_buffer_new ();
+  mem = gst_memory_new_wrapped (GST_MEMORY_FLAG_NO_SHARE, data_tensors,
+      size_tensors, 0, size_tensors, data_tensors, g_free);
+  gst_buffer_append_memory (buf, mem);
+  *buffer = buf;
+  silent_debug (rossrc, "Buffer of TensorRosSrc is pushed! (queue size: %d)\n",
+    g_async_queue_length (rossrc->queue));
+
+  return GST_FLOW_OK;
+}
+
+/**
+ * @brief Initialize the tensor_ros_src plugin
+ */
+static gboolean
+gst_tensor_ros_src_plugin_init (GstPlugin * rossrc)
+{
+  GST_DEBUG_CATEGORY_INIT (gst_tensor_ros_src_debug,
+      G_STRINGIFY (NNS_NAME_TENSOR_ROS_SRC), 0,
+      "Source element to get the topic data from ROS node");
+
+  return gst_element_register (rossrc, G_STRINGIFY (NNS_NAME_TENSOR_ROS_SRC),
+      GST_RANK_NONE, GST_TYPE_TENSOR_ROS_SRC);
+}
+
+#ifndef PACKAGE
+#define PACKAGE "nnstreamer-ros"
+#endif
+
+/**
+ * @brief Macro to define the entry point of the plugin.
+ */
+GST_PLUGIN_DEFINE (
+    GST_VERSION_MAJOR,
+    GST_VERSION_MINOR,
+    NNS_NAME_TENSOR_ROS_SRC,
+    "Source element to get the topic data from ROS node",
+    gst_tensor_ros_src_plugin_init,
+    NNS_VERSION,
+    "LGPL",
+    "nnstreamer-ros",
+    "https://github.com/nnstreamer/nnstreamer-ros"
+)
---
+++ ros/node/nns_roscpp_subscriber.cc
@@ -0,0 +1,317 @@
+/* SPDX-License-Identifier: LGPL-2.1-only */
+/**
+ * NNStreamer-ROS: NNStreamer extension packages for ROS/ROS2 support
+ * Copyright (C) 2019 Sangjung Woo <sangjung.woo@samsung.com>
+ * Copyright (C) 2020 Wook Song <wook16.song@samsung.com>
+ * Copyright (C) 2020 Samsung Electronics Co., Ltd.
+ *
+ * @file        nns_ros_subscriber.cc
+ * @date        03/06/2019
+ * @brief       A helper class for publishing ROS1 (i.e., roscpp) topic
+ *              via NNStreamer plugins
+ * @see         https://github.com/nnstreamer/nnstreamer-ros
+ * @author      Sangjung Woo <sangjung.woo@samsung.com>
+ *              Wook Song <wook16.song@samsung.com>
+ * @bug         No known bugs except for NYI items
+ *
+ * This class bridges between the NNStreamer (C) and ROS1 frameworks
+ * (ROSCPP/C++) by implementing the NnsRosSubscriber class.
+ *
+ */
+
+#ifdef G_OS_WIN32
+#include <process.h>
+#else
+#include <sys/types.h>
+#include <unistd.h>
+#endif
+#include <glib.h>
+#include <glib/gstdio.h>
+#include <nnstreamer/tensor_typedef.h>
+
+#include <cstring>
+#include <chrono>
+#include <mutex>
+
+#include "ros/ros.h"
+#include "rosbag/bag.h"
+#include "std_msgs/MultiArrayDimension.h"
+#include "std_msgs/MultiArrayLayout.h"
+
+#include "nns_ros_bridge/Tensors.h"
+#include "nns_ros_subscriber.h"
+
+const char BASE_NODE_NAME[] = "tensor_ros_src";
+
+class NnsRosCppSubscriber
+    : public NnsRosSubscriber<nns_ros_bridge::Tensors>
+{
+public:
+  NnsRosCppSubscriber (const gchar *node_name, const char *topic_name,
+      const gdouble rate, const gdouble timeout, GstTensorsConfig *configs,
+      gboolean is_dummy);
+  ~NnsRosCppSubscriber ();
+
+  void subCallback (const std::shared_ptr<nns_ros_bridge::Tensors> msg) final;
+  void configure (const std::shared_ptr<nns_ros_bridge::Tensors> msg) final;
+  void finalize () final;
+
+private:
+  NnsRosCppSubscriber () {};
+  void loop ();
+
+  // Variables for ROS configuration
+  ros::NodeHandle *nh_parent;
+  ros::NodeHandle *nh_child;
+  ros::Subscriber ros_src_sub;
+  void subCallbackInternal (const nns_ros_bridge::Tensors msg);
+  void configureInternal (const nns_ros_bridge::Tensors msg);
+};
+
+template <>
+std::string NnsRosSubscriber<nns_ros_bridge::Tensors>::str_nns_helper_name =
+    "nns_roscpp_subscriber";
+
+/**
+ * @brief	The only public constructor of this class
+ * @param[in] node_name The name of the ROS node under /tensor_ros_sink
+ * @param[in] topic_name The name of topic what this class concerns
+ * @param[in] rate @todo
+ * @param[in] timeout @todo
+ * @param[in] configs @todo
+ * @param[in] is_dummy If TRUE, create the instance without roscore connection
+ * @return None
+ */
+NnsRosCppSubscriber::NnsRosCppSubscriber (const gchar *node_name,
+    const char *topic_name, const gdouble rate, const gdouble timeout,
+    GstTensorsConfig *configs, gboolean is_dummy)
+    : NnsRosSubscriber<nns_ros_bridge::Tensors> (node_name, topic_name,
+        rate, timeout, configs, is_dummy)
+{
+  std::chrono::_V2::system_clock::time_point st;
+  /* ROS initialization requires command-line arguments */
+  int dummy_argc = 0;
+  char **dummy_argv = NULL;
+  ros::Subscriber tmp_sub;
+
+  if (is_dummy) {
+    g_critical ("%s: Failed to create a NnsRosCppSubscriber insntance: Dummy mode is not supported yet\n",
+        (this->str_nns_helper_name).c_str ());
+    throw ROS_INVALID_OPTION;
+  }
+
+  if (getenv ("ROS_MASTER_URI") == NULL) throw ROS1_UNDEFINED_ROS_MASTER_URI;
+
+  ros::init (dummy_argc, dummy_argv, this->str_node_name);
+
+  if (!ros::master::check ()) throw ROS1_FAILED_TO_CONNECT_ROSCORE;
+
+  this->nh_parent = new ros::NodeHandle (BASE_NODE_NAME);
+  this->nh_child =
+      new ros::NodeHandle (*(this->nh_parent), this->str_node_name);
+
+  try {
+    tmp_sub = this->nh_child->subscribe (
+        this->str_sub_topic_name,
+        this->default_ros_queue_size,
+        &NnsRosCppSubscriber::configureInternal, this);
+  } catch (ros::Exception &e) {
+    g_critical ("%s: Failed to create a subscription for the given topic: %s",
+        (this->str_nns_helper_name).c_str (), e.what ());
+    throw ROS_FAILED_TO_CREATE_SUBSCRIPTION;
+  }
+
+  st = std::chrono::system_clock::now ();
+
+  ros::Rate ros_rate (this->rate);
+  while (ros::ok ()) {
+    std::chrono::duration<double> diff;
+
+    ros::spinOnce ();
+    ros_rate.sleep ();
+
+    {
+      std::unique_lock<std::mutex> lk (this->g_m);
+      if (this->is_configured)
+        break;
+    }
+
+    diff = std::chrono::system_clock::now () - st;
+    if (diff > this->timeout) {
+      g_critical ("%s: Failed to configure GstCaps from the given topic",
+          (this->str_nns_helper_name).c_str ());
+      throw ROS_SUBSCRIPTION_TIMED_OUT;
+    }
+  }
+
+  try {
+    this->ros_src_sub =
+        this->nh_child->subscribe (
+            this->str_sub_topic_name,
+            this->default_ros_queue_size,
+            &NnsRosCppSubscriber::subCallbackInternal, this);
+  } catch (ros::Exception &e) {
+    g_critical ("%s: Failed to create a subscription for the given topic: %s",
+        (this->str_nns_helper_name).c_str (), e.what ());
+    throw ROS_FAILED_TO_CREATE_SUBSCRIPTION;
+  }
+  this->looper = std::make_shared <std::thread> (&NnsRosCppSubscriber::loop,
+    this);
+  {
+    std::lock_guard<std::mutex> lk (this->looper_m);
+    this->is_looper_ready = true;
+  }
+  this->looper_cv.notify_all ();
+}
+
+NnsRosCppSubscriber::~NnsRosCppSubscriber ()
+{
+  ros::shutdown ();
+}
+
+void
+NnsRosCppSubscriber::finalize ()
+{
+  {
+    std::unique_lock<std::mutex> lk (this->g_m);
+    this->is_looper_ready = false;
+    this->is_configured = false;
+  }
+  this->looper->join ();
+  g_async_queue_unref (this->gasyncq);
+}
+
+void
+NnsRosCppSubscriber::configure (
+    const std::shared_ptr<nns_ros_bridge::Tensors> msg)
+{
+  guint num_tensors = static_cast<guint> (msg->tensors.size ());
+
+  this->configs->info.num_tensors = num_tensors;
+  this->configs->rate_n = this->rate * 10000;
+  this->configs->rate_d = 10000;
+
+  for (guint i = 0; i < num_tensors; ++i) {
+    GstTensorInfo *info = &(this->configs->info.info[i]);
+    std_msgs::MultiArrayLayout layout = msg->tensors[i].layout;
+
+    for (guint j = 0; j < NNS_TENSOR_RANK_LIMIT; ++j) {
+      guint nns_dim = NNS_TENSOR_RANK_LIMIT - 1 - j;
+
+      info->dimension[nns_dim] = static_cast<guint> (layout.dim[j].size);
+      info->type = gst_tensor_get_type (layout.dim[j].label.c_str ());
+    }
+  }
+
+  std::unique_lock<std::mutex> lk (this->g_m);
+  this->is_configured = true;
+}
+
+void
+NnsRosCppSubscriber::subCallback (
+    const std::shared_ptr<nns_ros_bridge::Tensors> msg)
+{
+  guint num_tensors = this->configs->info.num_tensors;
+  guint size_tensors = 0;
+
+  for (guint i = 0; i < num_tensors; ++i) {
+    gsize size = gst_tensor_info_get_size (&(this->configs->info.info[i]));
+
+    size_tensors += size;
+  }
+
+  gpointer data = g_malloc0 (size_tensors);
+
+  for (guint i = 0; i < num_tensors; ++i) {
+    uint8_t *off = (uint8_t *) data;
+    gsize size = gst_tensor_info_get_size (&(this->configs->info.info[i]));
+
+    std::memcpy (off, msg->tensors.at (i).data.data (), size);
+    off += size;
+  }
+  g_async_queue_push (this->gasyncq, data);
+}
+
+void
+NnsRosCppSubscriber::subCallbackInternal (const nns_ros_bridge::Tensors msg)
+{
+  auto msgSharedPtr = std::make_shared<nns_ros_bridge::Tensors> (msg);
+
+  this->subCallback (msgSharedPtr);
+}
+
+void
+NnsRosCppSubscriber::configureInternal (const nns_ros_bridge::Tensors msg)
+{
+  auto msgSharedPtr = std::make_shared<nns_ros_bridge::Tensors> (msg);
+
+  this->configure (msgSharedPtr);
+
+}
+
+void
+NnsRosCppSubscriber::loop ()
+{
+  ros::Rate ros_rate (this->rate);
+  std::unique_lock<std::mutex> lk (this->looper_m);
+
+  this->looper_cv.wait (lk,
+      [&] () -> gboolean
+      {
+        return this->is_looper_ready;
+      }
+  );
+
+  while (true) {
+    ros::spinOnce ();
+    ros_rate.sleep ();
+    {
+      std::unique_lock<std::mutex> lk (this->g_m);
+      if (!this->is_looper_ready)
+        break;
+    }
+  }
+}
+
+/**
+ * C functions for NNStreamer plugins that want to publish or subscribe
+ * ROS2 topics. nns_ros_subscriber_init() should be invoked before configuring the
+ * tensors information to publish or subscribe. Each NNStreamer plugin which
+ * uses this class should be holding the instance of NnsRclCppSubscriber (i.e.,
+ * the void type pointer what the nns_ros_subscriber_init() function returns).
+ */
+void *nns_ros_subscriber_init (const gchar *node_name, const char *topic_name,
+      const gdouble rate, const gdouble timeout, GstTensorsConfig *configs,
+      gboolean is_dummy)
+{
+  try {
+    return new NnsRosCppSubscriber (node_name, topic_name, rate, timeout,
+        configs, is_dummy);
+  } catch (const err_code e) {
+    return nullptr;
+  }
+}
+
+void nns_ros_subscriber_fianlize (void *instance)
+{
+  NnsRosCppSubscriber *nrs_instance = (NnsRosCppSubscriber *) instance;
+
+  nrs_instance->finalize ();
+
+  if (nrs_instance)
+    delete nrs_instance;
+}
+
+GAsyncQueue *nns_ros_subscriber_get_queue (void *instance)
+{
+  NnsRosCppSubscriber *nrs_instance = (NnsRosCppSubscriber *) instance;
+
+  return nrs_instance->getQueue ();
+}
+
+void nns_ros_subscriber_put_queue (void *instance)
+{
+  NnsRosCppSubscriber *nrs_instance = (NnsRosCppSubscriber *) instance;
+
+  nrs_instance->putQueue ();
+}
