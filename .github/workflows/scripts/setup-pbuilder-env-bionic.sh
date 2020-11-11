40eccb6234b5d8af74650f2f5a704d76e27ddf7a
Wook Song <wook16.song@samsung.com>
2020-11-11 13:52:58 +0900

    [GitHub/Actions] Add a GitHub workflow for pdebuild
    This patch adds a GitHub Actions workflow for pdebuild.
    
    Signed-off-by: Wook Song <wook16.song@samsung.com>

---
+++ .github/workflows/scripts/setup-pbuilder-env-bionic.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/env bash
+sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key CADA0F77901522B3
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+cp -f .github/workflows/conf/pbuilderrc-bionic.conf $HOME/.pbuilderrc
+sudo apt update
+sudo apt install pbuilder curl debootstrap devscripts -y
+sudo pbuilder create
+echo "apt install -y lsb-release debian-keyring ubuntu-keyring curl gnupg2 debhelper cmake" \
+	> sudo pbuilder --login --save-after-exec
